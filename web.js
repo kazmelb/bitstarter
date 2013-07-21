9481805900fa80edb6ad48098d912811fc785f7c
Karen Keefe <karen@karen-keefe.net>
2013-07-21 11:12:12 +0000

    editing files

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
-app.use(express.logger());
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.write());
+  response.send('hello');
 });
 
 var port = process.env.PORT || 5000;
@@ -12,7 +12,3 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-var index = fs.readFileSync('index.html', function(err, data) {
- if (err) throw err;
- return new Buffer(data);
-});
