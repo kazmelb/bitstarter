4dc7bacfcfacf2dfbd0e3403cb861b7ff3221104
Karen Keefe <karen@karen-keefe.net>
2013-07-21 10:55:41 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index.write());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+var index = fs.readFileSync('index.html', function(err, data) {
+ if (err) throw err;
+ return new Buffer(data);
 });
